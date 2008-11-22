1b88d73b81984a3ee63b6b3c934e10b7fd7636f1
gray <gray@cpan.org>
2008-11-22 07:42:30 +0000

    Import of GRAY/Crypt-Skip32-XS-0.01 from CPAN.
    gitpan-cpan-distribution: Crypt-Skip32-XS
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         GRAY/Crypt-Skip32-XS-0.01.tar.gz
    gitpan-cpan-author:       GRAY
    gitpan-cpan-maturity:     released

---
+++ bench/benchmark.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Benchmark qw(cmpthese timethese);
+use Crypt::Skip32;
+use Crypt::Skip32::XS;
+
+my $key  = pack("H20", "112233445566778899AA");
+my $text = pack("N", 3493209676);
+
+my $pp = Crypt::Skip32->new($key);
+my $xs = Crypt::Skip32::XS->new($key);
+
+my $benchmarks = timethese -1, {
+    pp => sub { $pp->decrypt($pp->encrypt($text)) },
+    xs => sub { $xs->decrypt($xs->encrypt($text)) },
+};
+
+cmpthese $benchmarks;
