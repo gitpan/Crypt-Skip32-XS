c6ff259e5214eeaba820dd432783ef774eaa82c4
gray <gray@cpan.org>
2009-07-30 01:27:41 +0000

    Import of GRAY/Crypt-Skip32-XS-0.05 from CPAN.
    gitpan-cpan-distribution: Crypt-Skip32-XS
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         GRAY/Crypt-Skip32-XS-0.05.tar.gz
    gitpan-cpan-author:       GRAY
    gitpan-cpan-maturity:     released

--- lib/Crypt/Skip32/XS.pm
+++ lib/Crypt/Skip32/XS.pm
@@ -3,7 +3,7 @@ package Crypt::Skip32::XS;
 use strict;
 use warnings;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 eval {
     require XSLoader;
@@ -117,7 +117,7 @@ You can also look for information at:
 
 =item * GitHub Source Repository
 
-L<http://github.com/gray/geo-coder-bing>
+L<http://github.com/gray/crypt-skip32-xs>
 
 =item * AnnoCPAN: Annotated CPAN documentation
 
